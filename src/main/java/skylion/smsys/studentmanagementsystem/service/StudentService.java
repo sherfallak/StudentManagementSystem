b825606c95ffa101c27e7b31e422ff796e30db73
sherfallak <sh.fallak@gmail.com>
2022-03-01 22:15:54 +0100

    Created student Service and implementing class. closes #6

---
+++ src/main/java/skylion/smsys/studentmanagementsystem/service/StudentService.java
@@ -0,0 +1,4 @@
+package skylion.smsys.studentmanagementsystem.service;
+
+public interface StudentService {
+}
---
+++ src/main/java/skylion/smsys/studentmanagementsystem/service/impl/StudentServiceImpl.java
@@ -0,0 +1,6 @@
+package skylion.smsys.studentmanagementsystem.service.impl;
+
+import skylion.smsys.studentmanagementsystem.service.StudentService;
+
+public class StudentServiceImpl implements StudentService {
+}

dd029132bc302e07b1f4acbacfc1d2841d7e71c9
sherfallak <sh.fallak@gmail.com>
2022-03-01 22:09:44 +0100

    Created StudentRepository interface. closes #5

---
+++ src/main/java/skylion/smsys/studentmanagementsystem/repository/StudentRepository.java
@@ -0,0 +1,8 @@
+package skylion.smsys.studentmanagementsystem.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import skylion.smsys.studentmanagementsystem.entity.Student;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    //public List<Student> findAllStudent();
+}

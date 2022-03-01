128e9a2ebdba927fbdfa3234f62b3bb6b0e8ae5e
sherfallak <sh.fallak@gmail.com>
2022-03-01 23:21:32 +0100

    Added Handler Method in Controller Class to list All Students. closes #9

--- src/main/java/skylion/smsys/studentmanagementsystem/controller/StudentController.java
+++ src/main/java/skylion/smsys/studentmanagementsystem/controller/StudentController.java
@@ -1,7 +1,23 @@
 package skylion.smsys.studentmanagementsystem.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import skylion.smsys.studentmanagementsystem.service.StudentService;
 
 @Controller
 public class StudentController {
+    private StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        super();
+        this.studentService = studentService;
+    }
+
+    //Handler method to list all students
+    @GetMapping("/students")
+    public String listStudents(Model model) {
+        model.addAttribute("students", studentService.getAllStudent());
+        return "students";
+    }
 }

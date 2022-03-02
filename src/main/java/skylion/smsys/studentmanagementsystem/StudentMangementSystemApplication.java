6bb68c31e7b6e19e1a393dcca299600bafc71731
sherfallak <sh.fallak@gmail.com>
2022-03-02 01:23:03 +0100

    populated the student records. closes #11

--- src/main/java/skylion/smsys/studentmanagementsystem/StudentMangementSystemApplication.java
+++ src/main/java/skylion/smsys/studentmanagementsystem/StudentMangementSystemApplication.java
@@ -1,13 +1,31 @@
 package skylion.smsys.studentmanagementsystem;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import skylion.smsys.studentmanagementsystem.entity.Student;
+import skylion.smsys.studentmanagementsystem.repository.StudentRepository;
 
 @SpringBootApplication
-public class StudentMangementSystemApplication {
+public class StudentMangementSystemApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(StudentMangementSystemApplication.class, args);
     }
 
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Student student1 = new Student("fallak", " sher", "fsmalik@gmail.com");
+        studentRepository.save(student1);
+
+        Student student2 = new Student("sheraz", " anjum", "sanjum@gmail.com");
+        studentRepository.save(student2);
+
+        Student student3 = new Student("zain", " bilal", "zainbilal@gmail.com");
+        studentRepository.save(student3);
+    }
 }

9418b429f9f46af4289fd2a1227e487c24a81664
sherfallak <sh.fallak@gmail.com>
2022-03-02 23:32:51 +0100

    added handler method controller and created add new student view . closes #12, 13

--- src/main/java/skylion/smsys/studentmanagementsystem/controller/StudentController.java
+++ src/main/java/skylion/smsys/studentmanagementsystem/controller/StudentController.java
@@ -3,6 +3,7 @@ package skylion.smsys.studentmanagementsystem.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import skylion.smsys.studentmanagementsystem.entity.Student;
 import skylion.smsys.studentmanagementsystem.service.StudentService;
 
 @Controller
@@ -19,5 +20,14 @@ public class StudentController {
     public String listStudents(Model model) {
         model.addAttribute("students", studentService.getAllStudent());
         return "students";
+    }
+
+    //Handler method to create Student Form
+    @GetMapping("/students/new")
+    public String createStudentForm(Model model) {
+        //create student object to hold student form data
+        Student student = new Student();
+        model.addAttribute("student", student);
+        return "create_student";
     }
 }
